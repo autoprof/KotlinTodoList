600cb61229b82ffd8534f15db6098ee555606e16
dader <dader@dader>
2021-04-08 03:21:22 +0200

    final

--- app/src/main/java/net/syntessense/app/myapplication/MainActivity.kt
+++ app/src/main/java/net/syntessense/app/myapplication/MainActivity.kt
@@ -2,10 +2,53 @@ package net.syntessense.app.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.util.SparseBooleanArray
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var itemlist = arrayListOf<String>()
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemlist)
+
+        add.setOnClickListener {
+            itemlist.add(editText.text.toString())
+            listView.adapter = adapter
+            adapter.notifyDataSetChanged()
+            // This is because every time when you add the item the input      space or the eidt text space will be cleared
+            editText.text.clear()
+        }
+
+        delete.setOnClickListener {
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item = count - 1
+            while (item >= 0) {
+                if (position.get(item)) {
+                    adapter.remove(itemlist.get(item))
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        clear.setOnClickListener {
+
+            itemlist.clear()
+            adapter.notifyDataSetChanged()
+
+        }
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            android.widget.Toast.makeText(
+                    this,
+                    "You Selected the item --> " + itemlist.get(i), android.widget.Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
