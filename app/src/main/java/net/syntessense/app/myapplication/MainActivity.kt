ad58a5d674d4cab10556d11c988bcce2459217de
dader <dader@dader>
2021-04-13 16:45:09 +0200

    add ROOM Database

--- app/src/main/java/net/syntessense/app/myapplication/MainActivity.kt
+++ app/src/main/java/net/syntessense/app/myapplication/MainActivity.kt
@@ -4,49 +4,82 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.util.SparseBooleanArray
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import net.syntessense.app.myapplication.model.TodoDB
+import net.syntessense.app.myapplication.model.TodoEntity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var itemlist = arrayListOf<String>()
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemlist)
 
-        add.setOnClickListener {
-            itemlist.add(editText.text.toString())
-            listView.adapter = adapter
-            adapter.notifyDataSetChanged()
-            // This is because every time when you add the item the input      space or the eidt text space will be cleared
-            editText.text.clear()
+        val TODOS = Room.databaseBuilder(
+            applicationContext,
+            TodoDB::class.java, "todo-list.db"
+        ).build().todoDao()
+
+        var todoList = arrayListOf<String>()
+        var todoIdList = arrayListOf<Int>()
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, todoList)
+        listView.adapter = adapter
+
+        fun refresh() {
+            val todos = TODOS.getAll()
+            todoList.clear()
+            todoIdList.clear()
+            for (todo in todos) {
+                todoList.add(todo.content)
+                todoIdList.add(todo.id)
+            }
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                adapter.notifyDataSetChanged()
+            })
         }
 
-        delete.setOnClickListener {
-            val position: SparseBooleanArray = listView.checkedItemPositions
-            val count = listView.count
-            var item = count - 1
-            while (item >= 0) {
-                if (position.get(item)) {
-                    adapter.remove(itemlist.get(item))
-                }
-                item--
+        GlobalScope.launch {
+            refresh()
+        }
+
+        add.setOnClickListener {
+            val txt = editText.text.toString();
+            editText.text.clear()
+            GlobalScope.launch{
+                TODOS.add(TodoEntity(0, txt))
+                refresh()
             }
-            position.clear()
-            adapter.notifyDataSetChanged()
         }
 
         clear.setOnClickListener {
+            GlobalScope.launch {
+                TODOS.clear()
+                refresh()
+            }
+        }
 
-            itemlist.clear()
-            adapter.notifyDataSetChanged()
-
+        delete.setOnClickListener {
+            GlobalScope.launch {
+                val position: SparseBooleanArray = listView.checkedItemPositions
+                val count = listView.count
+                var i = count - 1
+                while (i >= 0) {
+                    if (position.get(i)) {
+                        TODOS.remove(todoIdList[i])
+                    }
+                    i--
+                }
+                position.clear()
+                refresh()
+            }
         }
 
         listView.setOnItemClickListener { adapterView, view, i, l ->
             android.widget.Toast.makeText(
                     this,
-                    "You Selected the item --> " + itemlist.get(i), android.widget.Toast.LENGTH_SHORT
+                    "You Selected the item --> " + todoIdList[i].toString(), android.widget.Toast.LENGTH_SHORT
             ).show()
         }
 
